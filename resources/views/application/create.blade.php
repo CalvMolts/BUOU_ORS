eca43155de9a811817d1aaf4785f71d30f384885
CalvMolts <calvinesguerra.molata@bicol-u.edu.ph>
2022-07-28 22:09:39 +0800

    test5

--- resources/views/application/create.blade.php
+++ resources/views/application/create.blade.php
@@ -20,7 +20,7 @@
 
     <div class="card-header">Add Application</div>
 
-<!-- Start Forms -->
+<!-- Start Formss -->
         <div class="card">
           <form method="post" action="{{ route('application.store'
           ) }}" enctype="multipart/form-data">
